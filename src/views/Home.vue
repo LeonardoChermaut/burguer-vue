23b1131029cd2daf7efd0bdd9277aa18f9d097c5
Leonardo Chermaut <chermauts@hotmail.com>
2023-08-31 14:55:53 -0300

    feat: impl views app

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <BannerLogoBurguerComponent />
+    <div class="main-home-conteiner">
+        <h1>Monte seu Hamburguer</h1>
+        <FormOrderComponent />
+    </div>
+</template>
+<script lang="ts">
+import BannerLogoBurguerComponent from '../components/Banner.vue';
+import FormOrderComponent from "../components/FormOrder.vue"
+
+export default {
+    name: 'HomeView',
+    components: {
+        BannerLogoBurguerComponent,
+        FormOrderComponent
+    }
+
+}
+</script>
---
+++ src/views/Order.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="maint-order-container">
+      <h1>Pedidos</h1>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    name: 'OrderView'
+  }
+  </script>
+  
+  <style scoped></style>
