e636c1a9181a6e32eed0ce6764ccb44cd3434b79
Leonardo Chermaut <chermauts@hotmail.com>
2023-09-03 16:21:08 -0300

    feat: add dashboard component

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export default {
+        name: "DashboardComponent"
+    }
+
+</script>
+
+<template>
+    <h1>Componente de dashboard</h1>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,13 +1,19 @@
+<script lang="ts">
+import DashboardComponent from '../components/Dashboard.vue';
+export default {
+  name: 'OrderView',
+  components: {
+    DashboardComponent
+  }
+  
+}
+</script>
+
 <template>
     <div id="maint-order-container">
-      <h1>Pedidos</h1>
+      <h1>Gerenciar de Pedidos</h1>
+      <DashboardComponent />
     </div>
   </template>
-  
-  <script lang="ts">
-  export default {
-    name: 'OrderView'
-  }
-  </script>
-  
+
   <style scoped></style>
